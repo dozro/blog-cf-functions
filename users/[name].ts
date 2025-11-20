57fd2752975d544d9ec4437b78eb13beebb46a6b
Rye <git@itsrye.dev>
2025-11-20 02:50:38 +0100

    updated endpoint

--- users/[name].ts
+++ users/[name].ts
@@ -9,6 +9,10 @@ export async function onRequestGet({ params, env }) {
     "id": `https://about.itsrye.dev/users/${username}`,
     "type": "Person",
     "preferredUsername": username,
+    "name": "Rye's Blog",
+    "summary": "Personal blog of Rye",
+    "discoverable": true,
+    "indexable": true,
     "inbox": `https://about.itsrye.dev/users/${username}/inbox`,
     "outbox": `https://about.itsrye.dev/users/${username}/outbox`,
     "followers": `https://about.itsrye.dev/users/${username}/followers`,
@@ -17,7 +21,34 @@ export async function onRequestGet({ params, env }) {
       "type": "Image",
       "mediaType": "image/png",
       "url": "https://about.itsrye.dev/images/avatar.png"
-    }
+    },
+    "attachment": [
+      {
+        "type": "PropertyValue",
+        "name": "mastodon",
+        "value": "@rye@catcatnya.com"
+      },
+      {
+        "type": "PropertyValue",
+        "name": "github",
+        "value": "https://github.com/dozro"
+      },
+      {
+        "type": "PropertyValue",
+        "name": "main mastodon account",
+        "value": "<p><a href=\"https://catcatnya.com/@rye\">https://catcatnya.com/@rye</a></p>"
+      },
+    ],
+    "_misskey_requireSigninToViewContents": false,
+    "enableRss": true,
+    "vcard:Address": "Europe",
+    "listenbrainz": "ryelavaplanetnixeris",
+    "backgroundUrl": {
+      "type": "Image",
+      "url": "https://about.itsrye.dev/images/background.webp",
+      "sensitive": false,
+      "name": "background"
+    },
   };
 
   return new Response(JSON.stringify(actor), {
